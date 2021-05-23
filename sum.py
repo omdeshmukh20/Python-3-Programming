dbc8d330d5450fc8fb6190302213c767bf7bcc71
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-05-23 14:52:57 +0530

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,15 @@
+def main():
+	arr=[]
+	print("Enter no of elements")
+	size=int(input())
+
+	for i in range(size):
+		print("The element no is:",i+1)
+		no=int(input())
+		arr.append(no)
+	
+
+	print("your entered elements are:",arr)	
+	print("the sum of ur entered elements is:",sum(arr))
+if __name__ == '__main__':
+	main()

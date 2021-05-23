dd816ad32cd43a14242161604140f94db32dbcdd
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-05-23 15:07:11 +0530

    Complete.py

---
+++ complete.py
@@ -0,0 +1,13 @@
+
+
+print("Enter first number:")
+value1 = int(input())
+
+print("enter second number:")
+value2 = int(input())
+
+ret1=value1,value2
+ret2=value1,value2
+
+print("Addition is :",ret1)
+print("Substraction is :",ret2)

650f7b7b0d289ccd40f88ac9547b0162673f989a
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-05-23 15:11:34 +0530

    Add files via upload

---
+++ Iteration1.py
@@ -0,0 +1,25 @@
+
+#Sequence approach
+def StartS():
+	print("Jay Ganesh")
+	print("Jay Ganesh")
+	print("Jay Ganesh")
+	print("Jay Ganesh")
+	print("Jay Ganesh")
+
+#Iterative approach
+def Start1():
+	iCnt=0
+	while iCnt<5:
+		print("Jay Ganesh")
+		iCnt=iCnt+1
+
+def main():
+	print("Output by sequence")
+	StartS()
+	print("Output by Iteration")
+	Start1()
+
+
+if __name__ == '__main__':
+			main()		

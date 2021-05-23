59b2ddca4d0323e456af37aacb43fe5b685bd4ac
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-05-23 15:13:01 +0530

    Check even

---
+++ Selection1.py
@@ -0,0 +1,20 @@
+def CheckEven(No):
+	if No%2==0:
+		return True
+	else:
+		return False
+			
+def main():
+	print("enter one number")
+	value=int(input())
+
+	bret=checkEven(value)
+
+	if bret==True:
+		print("{} is Even Number".format(value))
+	else:
+	    print("{} is odd Number".format(value))	
+
+
+if __name__ == '__main__':
+	main()

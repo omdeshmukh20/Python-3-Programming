23f6c96481914f408e4eeec1a26d70f0fd16131c
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-06-09 09:13:30 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,19 @@
+#Function name:
+#Discription: See the type of functions
+#input: 11 , 12.45 , 4+3j
+#output: a , b , or c
+#Date: 9/06/21
+#Author : Om Deshmukh
+
+a=11
+print("a")
+print(type(a))
+
+b=12.45
+print("b")
+print(type(b))
+
+c=4+3j
+print("c")
+print(type(c))
+

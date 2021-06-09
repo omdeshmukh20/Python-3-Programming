badecbe26cc22ba962493a7cbc004fa6efeeaf55
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-06-09 09:20:58 +0530

    Create Getsize.py

---
+++ Getsize.py
@@ -0,0 +1,12 @@
+#Discription: sys.get size of number
+#input: 27
+#output: 27,<class 'int'>,28
+#Date: 9/06/21
+#Author : Om Deshmukh
+
+import sys
+
+a=27
+print(a)
+print(type(a))
+print(sys.getsizeof(a))

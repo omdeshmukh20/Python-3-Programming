42acbddd15c8b11f3a1758285c292f666d976d13
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-06-09 09:28:06 +0530

    Create Addition.py

---
+++ Addition.py
@@ -0,0 +1,12 @@
+#Discription: Addition of two numbers
+#input: 11,5
+#output: The adition is: 16
+#Date: 9/06/21
+#Author : Om Deshmukh
+
+def addition(value1, value2):
+    ret=value1+value2
+    return ret
+
+ret=addition(11,5)
+print("The adition is:",ret)

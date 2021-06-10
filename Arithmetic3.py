77f483640e677a09d005881966ad86bc077f38aa
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-06-10 18:32:46 +0530

    Addition of two numbers using function

---
+++ Arithmetic3.py
@@ -0,0 +1,19 @@
+#Discription: Addition of two numbers using function
+#input: From user
+#output: Expected to user
+#Date: 10/06/21
+#Author : Om Deshmukh
+
+ def addition(value1, value2):
+    ret=value1 + value2
+    return ret
+no1=int(input("Enter first number"))
+no2=int(input("enter second number"))
+ans=Addition(no1,no2)
+print("additon is",ans)
+
+no1=int(input("Enter first number"))
+no2=int(input("enter second number"))
+ans=Addition(no1,no2)
+print("additon is",ans)
+

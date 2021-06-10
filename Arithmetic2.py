829a6a2492845cf699451ca4b7ae1d1c62706456
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-06-10 18:26:57 +0530

    Addition of two numbers acccording to user.

---
+++ Arithmetic2.py
@@ -0,0 +1,11 @@
+#Discription: Addition of two numbers according to user.
+#input: From user
+#output: Expected to user
+#Date: 10/06/21
+#Author : Om Deshmukh
+
+
+no1=int(input("Enter first number"))
+no2=int(input("enter second number"))
+ans=no1+no2
+print("additon is",ans)

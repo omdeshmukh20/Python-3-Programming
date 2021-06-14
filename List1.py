3e0f42e6580ee25bc5f1b650d0eb1ed60b195ee5
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-06-14 22:46:42 +0530

    List of numbers

---
+++ List1.py
@@ -0,0 +1,15 @@
+#Discription: List of numbers
+#Date: 14/06/21
+#Author : Om Deshmukh
+
+def main():
+	Arr=[10,20,30,40,50]
+
+	print(Arr)
+	print(Arr[3])
+
+	Brr=[10,"Marvellous",66.89,"Pune"]
+	print(Brr)
+
+if __name__ == '__main__':
+		main()	

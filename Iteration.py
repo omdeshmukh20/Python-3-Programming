bbe56ab775036e45c21010d95f0db4bdacb3c8d5
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-06-14 22:43:27 +0530

    Sequence approach and Iterative approach

---
+++ Iteration.py
@@ -0,0 +1,28 @@
+#Discription:Sequence approach and Iterative approach
+#Date: 14/06/21
+#Author : Om Deshmukh
+
+#Sequence approach
+def StartS():
+	print("Jay Ganesh")
+	print("Jay Ganesh")
+	print("Jay Ganesh")
+	print("Jay Ganesh")
+	print("Jay Ganesh")
+
+#Iterative approach
+def Start1():
+	iCnt=0
+	while iCnt<5:
+		print("Jay Ganesh")
+		iCnt=iCnt+1
+
+def main():
+	print("Output by sequence")
+	StartS()
+	print("Output by Iteration")
+	Start1()
+
+
+if __name__ == '__main__':
+			main()		

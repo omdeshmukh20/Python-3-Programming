6ec6df98f875a7c8f821944f912af952058b3b13
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-06-16 22:16:31 +0530

    Addition

---
+++ Arithmetic7.py
@@ -0,0 +1,13 @@
+#Discription: Addition
+#Date: 14/06/21
+#Author : Om Deshmukh
+
+print("enter first number")
+no1=(11)
+
+
+print("ennter second number")
+no2=(5)
+
+ans=(no1)+(no2)
+print("addition is:",ans)

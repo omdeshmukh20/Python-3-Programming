f28bd1b4907b63ca009f5828eedb991dd9cc10a9
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-06-19 22:36:41 +0530

    Base 1,2 constructor and Derived constructor

---
+++ OOP6.py
@@ -0,0 +1,30 @@
+#Discription: Base 1,2 constructor and Derived constructor
+#Date: 19/06/21
+#Author : Om Deshmukh
+
+class Base1:
+    def __init__(self):
+        print("Inside Base1 constructior")
+    def fun(self):
+        print("Base1 fun")
+        
+class Base2:
+    def __init__(self):
+        print("Inside Base2 constructior")
+    def fun(self):
+        print("Base2 fun")
+        
+class Derived(Base2,Base1):
+    def __init__(self):
+        Base2.__init__(self)
+        Base1.__init__(self)
+        print("Inside Derived constructor")
+    def fun(self):
+        print("Derived fun")
+        
+def main():
+    dobj = Derived()
+    dobj.fun()
+    
+if __name__ == "__main__":
+    main()

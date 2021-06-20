0df4b087f25937c2a89ee6aacdd7615d82696fd9
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-06-20 22:15:19 +0530

    Substraction demo

---
+++ Decorator.py
@@ -0,0 +1,19 @@
+#Discription: Substraction demo
+#Date: 20/06/21
+#Author : Om Deshmukh
+def Subsraction(no1,no2):
+	return no1 - no2
+
+def main():
+	print("Enter first no")
+	value1=int(input())
+
+	print("Enter second no")
+	value2=int(input(00))
+
+	ret=Subsraction(value1,value2)
+
+	print("Subsraction is",ret)
+
+if __name__ == '__main__':
+		main()	

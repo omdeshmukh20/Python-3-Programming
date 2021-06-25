52e7652f07acb74fab6bee92c37d4f6527f2b603
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-06-25 06:32:36 +0530

    Practice addition

---
+++ Operator.py
@@ -0,0 +1,13 @@
+#Discription: Practice addition
+#Date: 25/06/21
+#Author : Om Deshmukh
+
+def main():
+	no1=10
+	no2=20
+
+	ret=no1+no2
+	print("The addition is:",ret)
+
+if __name__ == '__main__':
+		main()	

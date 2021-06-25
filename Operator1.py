bf510d00c1d2e290fa1ca6d1bef5bea42190ee71
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-06-25 06:33:51 +0530

    Addition of objects.

---
+++ Operator1.py
@@ -0,0 +1,22 @@
+#Discription: Addition of objects.
+#Date: 25/06/21
+#Author : Om Deshmukh
+
+class Demo:
+	def __init__(self,x,y):
+		self.i=x
+		self.j=y
+
+	def __add__(self,other):
+		return Demo(self.i+other.i,self.j+other.j)
+
+def main():
+	obj1 = Demo(10,20)
+	obj2 = Demo(30,40)
+
+	ret=obj1+obj2
+	print(ret.i,ret.j)
+	
+
+if __name__ == '__main__':
+	main()

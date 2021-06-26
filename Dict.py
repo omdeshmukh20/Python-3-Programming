2dedadd644ce9ec6d9c61157e86299945115b70b
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-06-26 07:01:01 +0530

    type , length of Dict

---
+++ Dict.py
@@ -0,0 +1,16 @@
+#Discription: type , length of Dict
+#Date: 26/06/21
+#Author : Om Deshmukh
+
+def main():
+	batches={"PPA":12500,"LB":11000,"Pyhton":13000,"Angular":10000}
+
+	print(type(batches))
+	print(len(batches))
+
+	print(batches)
+
+	print(batches["PPA"])
+
+if __name__ == '__main__':
+		main()	

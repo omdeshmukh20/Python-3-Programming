3ac1e34abf3b92a7346d3366d5d349032a8d0dc5
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-06-27 10:59:32 +0530

    Serially finding name of batch from directory.

---
+++ Dict3.py
@@ -0,0 +1,17 @@
+#Discription: Serialy finding name of batch from directory. 
+#Date: 27/06/21
+#Author : Om Deshmukh
+
+def main():
+    Employee = {11 : {"Name":"Piyush", "Age" : 30}, 21 : {"Name":"Amar", "Age":22}, 51 : {"Name": "Siddhi", "Age": 23}}
+        
+    print("Employee information is :")
+    
+    for eid,einformation in Employee.items():
+        print("Employee ID is ",eid)
+        
+        for key in einformation:
+            print(key, einformation[key])
+        
+if __name__ == "__main__":
+    main()

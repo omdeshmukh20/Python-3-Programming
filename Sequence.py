043d4e3f738682a73a45bc08535227e4d6ee7b08
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-06-27 11:03:35 +0530

    Type and range of arr

---
+++ Sequence.py
@@ -0,0 +1,14 @@
+#Discription: Type and range of arr 
+#Date: 27/06/21
+#Author : Om Deshmukh
+
+def main():
+
+	arr=(11,23,32.3,"hello")
+	print(type(arr))
+
+	for i in range(len(arr)):
+		print(arr[i])
+
+if __name__ == '__main__':
+	main()

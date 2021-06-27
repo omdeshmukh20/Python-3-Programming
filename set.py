07c72155e12dfaf49f9557add0da597360a2b08b
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-06-27 11:05:03 +0530

    SET class

---
+++ set.py
@@ -0,0 +1,13 @@
+#Discription: SET class 
+#Date: 27/06/21
+#Author : Om Deshmukh
+
+def main():
+	arr={10,1.5,"om"}
+
+	print(type(arr))
+	print(arr)
+	print(len(arr))
+	
+if __name__ == '__main__':
+		main()	

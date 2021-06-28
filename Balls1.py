94e7f4a8e809c11e2e8fec9d5a413e93d4c8525e
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-06-28 22:24:46 +0530

    Type of object

---
+++ Balls1.py
@@ -0,0 +1,24 @@
+#Discription: Type of object
+#Date: 28/06/21
+#Author : Om Deshmukh
+
+x=(1)
+print(x)
+print(type(x))
+
+#
+x=(1.6)
+print(x)
+print(type(x))
+
+#
+x=("hi")
+print(x)
+print(type(x))
+
+#
+x=(3-4j)
+print(x)
+print(type(x))
+
+

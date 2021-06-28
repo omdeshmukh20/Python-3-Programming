60624c5d1530adf086141b5c895c0e0807fcb6b4
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-06-28 22:07:26 +0530

    Type of object

---
+++ Tuple.py
@@ -0,0 +1,16 @@
+#Discription: Type of object
+#Date: 28/06/21
+#Author : Om Deshmukh
+
+def main():
+	arr=(10,20,30)
+	print("type of arr is",type(arr))
+
+	brr=(10)
+	print("type of brr is",type(brr))
+
+	crr=(10,)
+	print("type of crr is",type(crr))
+
+if __name__ == '__main__':
+		main()	

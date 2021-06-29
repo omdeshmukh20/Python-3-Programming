20ca09a9e217500ac4f3fcddf099af366701f738
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-06-29 18:57:32 +0530

    List

---
+++ practice3.py
@@ -0,0 +1,11 @@
+#Discription: List
+#Date: 29/06/21
+#Author : Om Deshmukh
+print("----By Om Deshmukh-----") 
+print("Demonstration of List") 
+batches = ["PPA","LB","Angular","Python"] 
+print(batches)
+print(batches[0])
+print(batches[-1])
+print(batches[3])
+print(batches[-4])

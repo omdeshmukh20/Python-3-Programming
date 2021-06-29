89ef6f46d39d13e6f23fa650e3a3c921efc9a005
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-06-29 18:58:28 +0530

    append list

---
+++ List6.py
@@ -0,0 +1,11 @@
+#Discription: append list
+#Date: 29/06/21
+#Author : Om Deshmukh
+batches.append("LSP")
+print(batches)
+
+batches.insert(2,"Mean")
+print(batches)
+
+batches.pop()
+print(batches)

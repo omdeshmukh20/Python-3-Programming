4d005d3b1aae24a8cc2cf4ecfa800fff7f03f3dd
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-06-29 18:58:01 +0530

    list of list

---
+++ List5.py
@@ -0,0 +1,13 @@
+#Discription: list of list
+#Date: 29/06/21
+#Author : Om Deshmukh
+print("---By Om Deshmukh---")
+data1=[1,"Heterogenious",3.27]
+print(data1)
+
+data2=[6161,"wtf",83883838383]
+print(data2)
+
+#List of list
+combine=[data1,data2]
+print(combine)

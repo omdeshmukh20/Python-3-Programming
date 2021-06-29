6d25639a78e6079927bff3a0097ce7795d1a2d72
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-06-29 19:00:08 +0530

    Function Arguement

---
+++ Function1.py
@@ -0,0 +1,13 @@
+
+#Discription: Function Arguement
+#Date: 29/06/21
+#Author : Om Deshmukh
+print("-----Marvellous infosystem------")
+print("Function Arguments")
+
+
+def Batche1(Name,Fee):
+	print("Batch Name is",Name)
+	print("Fees are",Fee)
+
+Batche1("python",5000)

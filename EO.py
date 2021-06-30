58cebd50829fb963f1c393446dd7ee8a9afa49ec
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-06-30 22:43:04 +0530

    Check Even odd

---
+++ EO.py
@@ -0,0 +1,11 @@
+#Discription: Check Even odd
+#Date: 30/06/21
+#Author : Om Deshmukh
+def ChkNum(value):
+	if value%2==0:
+		print("The number is even number")
+	else:
+		print("The number is odd number")
+
+ChkNum(11)
+ChkNum(8)

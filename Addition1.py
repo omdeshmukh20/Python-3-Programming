8c97ffc1abf92967a7b750cc4e367fe8d21705ed
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-06-30 22:43:58 +0530

    Add

---
+++ Addition1.py
@@ -0,0 +1,11 @@
+#Discription: Add
+#Date: 30/06/21
+#Author : Om Deshmukh
+def Add(value1,value2):
+	Add=value1+value2
+	return(Add)
+def main():
+	ret=Add(11,5)
+	print("Addition is",ret)
+if __name__ == '__main__':
+			main()	

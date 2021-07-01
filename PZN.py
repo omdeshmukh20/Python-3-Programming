8725daf26913a28fe32df1f0919a5e2218c91e50
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-07-01 21:25:28 +0530

    Positive, zero or negative number

---
+++ PZN.py
@@ -0,0 +1,13 @@
+#Discription: Positive, zero or negative number
+#Date: 1/07/21
+#Author : Om Deshmukh
+def checknum(Number):
+	if Number>0:
+		print("The number is positive")
+	elif Number==0:
+		print("The number is zero")
+	else:
+		print("The number is negative")	
+checknum(11)
+checknum(-8)
+checknum(0)

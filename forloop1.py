180384738c84f53b225ae37a1972c91162067280
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-07-05 18:04:34 +0530

    Output of for loop

---
+++ forloop1.py
@@ -0,0 +1,10 @@
+#Discription :Output of for loop
+#Date: 5/07/21
+#Author : Om Deshmukh
+def diaplay(value):
+	print("Output of for loop")
+	icnt=0
+	for i in range(0,value):
+
+		for j in range(0,value+i):
+			diaplay(5)

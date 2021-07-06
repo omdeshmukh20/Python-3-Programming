a587ec3631b27a81c90f2796df7ba05617ceba27
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-07-06 21:13:34 +0530

    division of two numbers

---
+++ Div.py
@@ -0,0 +1,12 @@
+#Discription: division of two numbers
+#Date: 06/07/21
+#Author : Om Deshmukh
+def divide(no1,no2):
+	divide=no1/no2
+	return(divide)
+def main():
+	ret=divide(10,2)
+	print("The division is:",ret)
+
+if __name__ == '__main__':
+		main()	

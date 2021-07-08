c47b46c3de4112d6ed18f8f78d057b05afde4376
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-07-08 18:30:41 +0530

    Check number

---
+++ Check.py
@@ -0,0 +1,10 @@
+#Discription :Check number
+#Date: 08/07/21
+#Author : Om Deshmukh
+def check(no):	
+	if no%5==0:
+		print("The no is divisible by 5")
+	else:
+		print("The no is not dividible by 5")
+
+check(1000)

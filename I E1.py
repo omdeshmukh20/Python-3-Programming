0c06bdf0587dcc2b70f390035fb0c726ef300bad
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-07-08 18:36:40 +0530

    If else

---
+++ I E1.py
@@ -0,0 +1,14 @@
+#Discription: If else
+#Date: 08/07/21
+#Author : Om Deshmukh
+def main():
+    print("Enter the num:")
+    num=int(input())
+
+    if num<10:
+        print("Hello")
+    else:
+        print("Demo")   
+
+if __name__ == '__main__':
+            main()     

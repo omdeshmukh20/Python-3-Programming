160628d2db1d978b1c9eda28f96a1a07c16748d4
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-07-08 18:35:54 +0530

    Pattern Printing

---
+++ pattern4.py
@@ -0,0 +1,12 @@
+#Discription: Pattern Printing
+#Date: 08/07/21
+#Author : Om Deshmukh
+def main():
+	print("enter the num:")
+	num=int(input())
+
+	for i in range(num):
+		print("* ",end="")
+
+if __name__ == '__main__':
+			main()	

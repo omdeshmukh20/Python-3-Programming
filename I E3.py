11e7db39a313ddd4a036d66b6f61521bed4c8eeb
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-07-09 22:07:01 +0530

    If else div

---
+++ I E3.py
@@ -0,0 +1,15 @@
+#Discription: If else div
+#Date: 09/07/21
+#Author : Om Deshmukh
+def main():
+    print("Enter no of elements:")
+    value=int(input())
+
+    if value%2==0:
+        print("The num is even")
+    else:
+        print("The num is odd")    
+
+if __name__ == '__main__':
+        main()  
+

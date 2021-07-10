cd6a61cff350302a08e5e4e290eec0e0ce6979cc
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-07-10 22:59:40 +0530

    str input

---
+++ string1.py
@@ -0,0 +1,11 @@
+#Discription: str input
+#Date: 10/07/21
+#Author : Om Deshmukh
+print("Enter the element:")
+element=str(input())    
+
+for i in range(element):
+	if element=str.upper():
+		print(element.lower())     
+	else element=str.lower():
+		print(element.upper())

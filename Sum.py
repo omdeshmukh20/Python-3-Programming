53accf781763e99d7f13aaa1cc0bf184a45843c7
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-07-10 23:04:52 +0530

    Sum of num in arr

---
+++ Sum.py
@@ -0,0 +1,15 @@
+#Discription: Sum of num in arr
+#Date: 10/07/21
+#Author : Om Deshmukh
+def main():
+	arr=[]
+	print("Enter the no of elements")
+	size=int(input())
+
+	for i in range(size):
+		print("The no of element is",i+1)
+		no=int(input())
+		arr.append(no)
+
+	print("The no of given elements are",arr)
+	print("The sum of given elements in arr is",sum(arr))

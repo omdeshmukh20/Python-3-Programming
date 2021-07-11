9e264d9c39b36c0f138cd45578a0e08952fab8e9
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-07-11 19:45:34 +0530

    Maximum digit num in arr

---
+++ Maximum.py
@@ -0,0 +1,19 @@
+#Discription: Maximum digit num in arr
+#Date: 11/07/21
+#Author : Om Deshmukh
+def main():
+	brr=[]
+	print("Enter the number of elements:")
+	no=int(input())
+
+	for i in range(no):
+		print("The entered element is:",i+1)
+		no=int(input())
+		brr.append(no)
+
+	print("The entered elements are",arr)
+	print("The maximun number of the enterd element is",max(arr))		
+
+
+if __name__ == '__main__':
+		main()	

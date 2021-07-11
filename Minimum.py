99967de836466ea93fcc573264c19dfb103c610e
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-07-11 19:46:31 +0530

    Minimum digit num in arr

---
+++ Minimum.py
@@ -0,0 +1,19 @@
+#Discription: Minimum digit num in arr
+#Date: 11/07/21
+#Author : Om Deshmukh
+def main():
+	crr=[]
+	print("Enter no of elements:")
+	no=int(input())
+
+	for i in range(no):
+		print("The no of elements is:",i+1)
+		no=int(input())
+		crr.append(no)
+
+	print("Entered number of elements is:",brr)
+	print("The minimum number in the list is:",min(brr))
+
+
+if __name__ == '__main__':
+		main()	

7b18826f0791c43fa19973375852c548b4d8c943
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-07-12 18:03:19 +0530

    N numbers from user

---
+++ N numbers.py
@@ -0,0 +1,19 @@
+
+
+
+
+#Discription: N numbers from user
+#Date: 12/07/21
+#Author : Om Deshmukh
+def main():
+	err=[]
+	print("Enter the number of elements:")
+	no=int(input())
+
+	for i in range(no):
+		print("enter the element",i+1)
+		no=int(input())
+		
+if __name__ == '__main__':
+		main()	
+

af7a95f04b496d0363bba47d179b2b6d5d910269
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-07-15 12:57:16 +0530

    checking if str is found in mainstring

---
+++ string2.py
@@ -0,0 +1,10 @@
+#Discription: Check string
+#Date: 15/07/21
+#Author : Om Deshmukh
+main=input("Enter a Main String:")
+sub=input("Enter a SubString:")
+
+if sub in main:
+	print(sub+"is Found is Main String")
+else:
+	print(sub+"is Not Found in Main String")

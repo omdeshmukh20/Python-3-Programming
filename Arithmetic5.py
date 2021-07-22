c30c322809bcfffb0871d05dc99ab9cc85e24e36
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-07-22 09:26:51 +0530

    Inside constructor

---
+++ Arithmetic5.py
@@ -0,0 +1,14 @@
+#Discription: Inside constructor
+#Date: 22/07/21
+#Author : Om Deshmukh
+
+class Arithematic:
+	def __init__(self):                                                                                                                                                                    
+		print("Inside Constructor")
+
+def main():
+	obj=Arithematic()
+	obj=Arithematic()
+	
+if __name__ == '__main__':
+		main()	

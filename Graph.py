779f055f65323844210ba042bd9d2cfc5fa04a71
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-07-27 10:12:14 +0530

    Making pie graph

---
+++ Graph.py
@@ -0,0 +1,9 @@
+#Discription: Making pie graph
+#Date: 27/07/21
+#Author : Om Deshmukh
+
+import matplotlib.pyplot as p
+s=[40,10,30,20]
+l=["Phython","C-Programming","JavaScript","HTML-CSS"]
+p.pie(s,labels=l)
+p.show()

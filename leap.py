ee2b33622a6afe4fde5581b99dd9f4b77ecbcca9
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-08-03 16:03:25 +0530

    Python Program to Check Leap Year using Elif Statement

---
+++ leap.py
@@ -0,0 +1,14 @@
+#Discription: Python Program to Check Leap Year using Elif Statement
+#Date: 03/08/21
+#Author : Om Deshmukh
+
+year = int(input("Please Enter the Year Number you wish: "))
+
+if (year%400 == 0):
+          print("%d is a Leap Year" %year)
+elif (year%100 == 0):
+          print("%d is Not the Leap Year" %year)
+elif (year%4 == 0):
+          print("%d is a Leap Year" %year)
+else:
+          print("%d is Not the Leap Year" %year)

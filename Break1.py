72fa925ec5548bfe62ec254ff4e3aa7be3568354
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-08-06 18:52:06 +0530

    break statement with while loop

---
+++ Break1.py
@@ -0,0 +1,13 @@
+
+
+#Discription: break statement with while loop
+#Date: 06/08/21
+#Author : Om Deshmukh
+
+i = 0;  
+while 1:  
+    print(i," ",end=""),  
+    i=i+1;  
+    if i == 10:  
+        break;  
+print("came out of while loop"); 

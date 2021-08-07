bd991f3b63310f66f413b3ef4c016a7f67198ddd
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-08-07 18:36:37 +0530

    Executing Sequence datatypes

---
+++ Datatypes2.py
@@ -0,0 +1,15 @@
+#Discription: Executing Sequence datatypes
+#Date: 07/08/21
+#Author : Om Deshmukh
+
+x="My name is Om"
+print(type(x))
+
+x=[12,10,0,20]
+print(type(x))
+
+x=(11,1.2,'om',12+3j)
+print(type(x))
+
+x=range(20)
+print(type(x))

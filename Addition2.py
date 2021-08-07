4dbaf4b2deeb42059f61f6d67eed0468f0adfe97
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-08-07 18:31:41 +0530

    Addition using function

---
+++ Addition2.py
@@ -0,0 +1,17 @@
+#Discription: Addition using function
+#Date: 07/08/21
+#Author : Om Deshmukh
+
+def add(no1,no2):
+	ret = no1+no2                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+	return(ret)
+
+def main():
+	no1=int(input("Enter first no"))
+	no2=int(input("Enter second no"))
+	ans=add(no1,no2)
+	print("The addition is:",ans)
+	
+
+if __name__ == '__main__':
+		main()	

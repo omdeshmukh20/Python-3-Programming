2c2feda25340bd028b2174a8063a60b00588c114
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-08-07 18:33:06 +0530

    Seeing the use of function

---
+++ Function2.py
@@ -0,0 +1,16 @@
+#Discription: Seeing the use of function
+#Date: 07/08/21
+#Author : Om Deshmukh
+
+def add(x,y):
+	print("Addition is:", (x+y))
+
+def message():
+	print("Welcom to python")
+
+def main():
+	add(12,12)
+	message()
+
+if __name__ == '__main__':
+			main()		

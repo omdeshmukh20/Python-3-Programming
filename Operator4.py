80888aeba9a357506618230b6e87683adb96664a
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-08-08 10:48:11 +0530

    Arithematic Operators

---
+++ Operator4.py
@@ -0,0 +1,23 @@
+#Discription: Arithematic Operators 
+#Date: 08/08/21
+#Author : Om Deshmukh
+x=(15+3)
+print(x)
+
+x=(15-3)
+print(x)
+
+x=(15*3)
+print(x)
+
+x=(15/3)
+print(x)
+
+x=(15%3)
+print(x)
+
+x=(15**3)
+print(x)
+
+x=(15//3)
+print(x)

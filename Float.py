dc12d724ed651508584109889f4a7d5dc515e03f
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-08-09 21:04:23 +0530

    How to write a float

---
+++ Float.py
@@ -0,0 +1,11 @@
+#Discription: How to write a float
+#Date: 09/08/21
+#Author : Om Deshmukh
+
+def main():
+
+	float=input("Enter the number :")
+	print("The entered float is =",float)
+
+if __name__ == '__main__':
+		main()	

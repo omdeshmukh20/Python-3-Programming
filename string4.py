c818b6e0460a1a22216011db8e253b23630bbcf6
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-08-09 20:58:24 +0530

    Entering the string

---
+++ string4.py
@@ -0,0 +1,11 @@
+#Discription: Entering the string
+#Date: 09/08/21
+#Author : Om Deshmukh
+
+def main():
+  
+	str=input("Enter the string")
+	print("The entered string is="+str)
+  
+if __name__ == '__main__':
+	main()

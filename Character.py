57c88723b3c6d1a0cd8d2a869ee9013ffcb7c576
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-08-09 21:03:05 +0530

    Entering the character

---
+++ Character.py
@@ -0,0 +1,11 @@
+##Discription: Entering the character
+#Date: 09/08/21
+#Author : Om Deshmukh
+
+def main():
+
+	ch=input("Enter the character:")
+	print("The entered char is=",ch[1])
+  
+if __name__ == '__main__':
+	main()

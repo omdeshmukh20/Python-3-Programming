e6e74ca9c4d8e00565700fb8e06e75440615fbfc
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-08-10 19:42:14 +0530

    Addition using function

---
+++ Addition3.py
@@ -0,0 +1,18 @@
+#Discription: Addition using function
+#Date: 10/08/21
+#Author : Om Deshmukh
+
+def add(no1,no2):
+	add=no1+no2
+	return add
+
+def main():
+	no1=int(input("Enter the number"))
+	no2=int(input("Enter the number"))
+	print("The entered numbers are",no1,"and",no2)
+	Ans=add(no1,no2)
+
+	print("The addition is",Ans)
+
+if __name__ == '__main__':
+		main()	

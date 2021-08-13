12c5cf4c319f0538cde9139716ad235aac8c2150
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-08-13 09:17:21 +0530

    find the factors of a number

---
+++ Factor3.py
@@ -0,0 +1,14 @@
+#Discription:  Python Program to find the factors of a number
+#Date: 11/08/21
+#Author : Om Deshmukh
+
+
+def print_factors(x):
+   print("The factors of",x,"are:")
+   for i in range(1, x + 1):
+       if x % i == 0:
+           print(i)
+
+num = 320
+
+print_factors(num)

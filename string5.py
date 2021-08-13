3f2e7640489054c50c97b878a2979701e989652d
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-08-13 09:21:45 +0530

    Python Program to Get a Substring of a String

---
+++ string5.py
@@ -0,0 +1,14 @@
+#Discription: Python Program to Get a Substring of a String
+#Date: 12/08/21
+#Author : Om Deshmukh
+
+my_string = "I love python."
+
+# prints "love"
+print(my_string[2:6])
+
+# prints "love python."
+print(my_string[2:])
+
+# prints "I love python"
+print(my_string[:-1])

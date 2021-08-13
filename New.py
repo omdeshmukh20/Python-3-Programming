33edeb05a7fc0607215fe1c8eabe647628a8cf9e
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-08-13 09:25:31 +0530

    print each statement on a new line

---
+++ New.py
@@ -0,0 +1,15 @@
+#Discription: print each statement on a new line
+#Date: 13/08/21
+#Author : Om Deshmukh
+
+
+# print each statement on a new line
+print("Python")
+print("is easy to learn.")
+
+# new line
+print()
+
+# print both the statements on a single line
+print("Python", end=" ")
+print("is easy to learn.")

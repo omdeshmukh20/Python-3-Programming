6472ce35d1ffa64c4d2cf6a0a3e55bd8ed64d6cd
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-08-19 12:44:41 +0530

    Understanding static method

---
+++ class3.py
@@ -0,0 +1,26 @@
+#Discription:# Python Program To Create A Static Method That Counts The Number Of Instances Created For A Class
+#Date: 19/08/21
+#Author : Om Deshmukh
+
+class Myclass:
+    
+    # This Is Class Variable Or Static Variable
+    
+    n = 0
+    
+    # Constructor That Increments n When An Instance Is Created
+    
+    def __init__(self):
+        Myclass.n = Myclass.n+1
+        
+    # This Is A Static Method To Display The no. Of Instances
+    @staticmethod
+    def noObjects():
+        print('No Of Instances Created : ', Myclass.n)
+        
+# Create 3 Instances
+
+obj1 = Myclass()
+obj2 = Myclass()
+obj3 = Myclass()
+Myclass.noObjects()

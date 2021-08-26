179ce9adef86019520a50279c0caa277cb4650f3
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-08-26 20:26:19 +0530

    Create Diagonal.py

---
+++ Diagonal.py
@@ -0,0 +1,15 @@
+  
+#Function Name:Diagonal
+#Input:--
+
+#Output:Diagonal Elements is [1,5,9]
+
+#Description:Find Diagonal Elements of matrix 
+#Date: 26/08/2021
+#Author: Om Deshmukh
+
+
+from numpy import *
+arr=[[1,2,3],[4,5,6],[7,8,9]]
+a=diagonal(arr)
+print("Diagonal Elements are:",a)

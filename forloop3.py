b60d4a10c3c1891a5ad3afbc8d2de8a9544ead6d
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-08-29 11:21:28 +0530

    Continue statement to print odd numbers till range

---
+++ forloop3.py
@@ -0,0 +1,9 @@
+#description: Continue statement to print odd numbers till range
+#date: 29-08-2021
+#Author name: Om deshmukh
+
+for x in range(20):
+   
+    if(x%2==0):
+        continue
+    print(x)

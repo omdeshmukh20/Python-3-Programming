33544f3ee7f176dba3595113cc846bb8f146abe3
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-09-01 22:36:26 +0530

    Use of break statement

---
+++ break.py
@@ -0,0 +1,15 @@
+  
+#Function Name:Break
+#Input:--
+#Output:10 9 8 7 6 
+#Description:Use of break statement
+#Date:01/09/2021
+#Author:Om Deshmukh
+
+x=10
+while x>=1:
+   print(x)
+   x=x-1
+   if x==5:
+   	break;
+print("End of Loop")

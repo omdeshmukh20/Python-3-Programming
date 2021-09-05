f6f8f7d2f240b9a1845057bb53e537c8051fadf2
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-09-05 10:11:20 +0530

    Transverse of string

---
+++ Transverse.py
@@ -0,0 +1,10 @@
+#Discription:Transverse of string
+#Date: 05/09/21
+#Author : Om Deshmukh
+
+msg="good morning"
+index=0
+while index<len(msg):
+     letter=msg[index]
+     print(letter)
+     index=index+1

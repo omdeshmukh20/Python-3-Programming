c4156566eab2be5dad38666d0115ea25268cbf82
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-09-06 14:16:59 +0530

    list concatenation

---
+++ Concatenation.py
@@ -0,0 +1,15 @@
+#Discription: list concatenation
+#Date: 06/09/21
+#Author : Om Deshmukh
+
+catNames = []
+while True:
+    print('Enter the name of cat ' + str(len(catNames) + 1) +
+      ' (Or enter nothing to stop.):')
+    name = input()
+    if name == '':
+        break
+    catNames = catNames + [name]               # list concatenation
+print('The cat names are:')
+for name in catNames:
+    print('  ' + name)

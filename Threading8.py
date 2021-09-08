812cea1877cdcb0cb8484b783a41344314a4168b
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-09-08 13:04:27 +0530

    Display Even And Odd Number Upto 10.

---
+++ Threading8.py
@@ -0,0 +1,28 @@
+#Discription : Display Even And Odd Number Upto 10.
+#Date : 08/09/21
+#Author : Om Deshmukh
+
+import threading
+
+def Even():
+    for i in range(1, 11):
+        if i % 2 == 0:
+            print("Even Numbers Are : ", i)
+def Odd():
+    for i in range(1, 10):
+        if i % 2 != 0:
+            print("Odd Numbers Are : ", i)
+
+def main():
+
+    thread1 = threading.Thread(target = Even, args = ())
+    thread2 = threading.Thread(target = Odd, args = ())
+
+    thread1.start() # It Execute Both Threads Parallel
+    thread2.start()
+
+    thread1.join()  # Wait Until Child Process Terminates & Then Return To Parent Process
+    thread2.join()
+
+if __name__ == "__main__":
+    main()

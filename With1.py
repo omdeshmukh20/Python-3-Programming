d713608bd979acb91cf307d9a239fc03d975f462
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-09-16 09:55:55 +0530

    Write Some Strings Into The File

---
+++ With1.py
@@ -0,0 +1,9 @@
+
+#Discription: Open A File And Write Some Strings Into The File
+#Date: 16/09/21
+#Author : Om Deshmukh
+
+
+with open('sample.txt', 'w') as f:
+    f.write('\n I Am Om Deshmukh')
+    f.write('Python Is The Attraction ')

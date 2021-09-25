61a3f57383bf5aecb1813ad8e29c137826e24a7c
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-09-25 21:53:25 +0530

    Create Student Class And Store It Into student

---
+++ Student.py
@@ -0,0 +1,29 @@
+#Discription: Python Program To Create Student Class And Store It Into student
+#Date: 25/09/21
+#Author : Om Deshmukh
+
+
+class student:
+    def setid(self, id):
+        self.id = id
+        
+    def getid(self):
+        return self.id
+    
+    def setname(self, name):
+        self.name = name
+        
+    def getname(self):
+        return self.name
+        
+    def setaddress(self, address):
+        self.address = address
+        
+    def getaddress(self):
+        return self.address
+    
+    def setmarks(self, marks):
+        self.marks = marks
+        
+    def getmarks(self):
+        return self.marks

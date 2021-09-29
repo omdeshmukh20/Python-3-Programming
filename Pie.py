6693cadd555ccfb1916c5fda046772c6c038f2f2
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-09-29 12:08:44 +0530

    Creates a PieGraph of Given Infornmations

---
+++ Pie.py
@@ -0,0 +1,9 @@
+#Description:Creates a PieGraph of Given Infornmations
+#Date: 29/09/2021
+#Author: Om Deshmukh
+
+import matplotlib.pyplot as p
+s=[40,10,30,20]
+l=["Phython","C-Programming","JavaScript","HTML-CSS"]
+p.pie(s,labels=l)
+p.show()

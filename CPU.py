d4b122dc8808e4bbef0fc87305d437cb0ea98711
Om Deshmukh <79681696+omdeshmukh20@users.noreply.github.com>
2021-10-14 09:58:33 +0530

    Demonstration of Serial Processing

---
+++ CPU.py
@@ -0,0 +1,34 @@
+'''
+
+Function Name    :  SerialProcessing()
+Description      :  Demonstration of Serial Processing
+Function Date    :  14 Oct 2021
+Function Author  :  Om Deshmukh
+
+...
+
+import os
+import time
+
+def Square(no):
+    return no * no
+
+def SerialProcessing():
+    start = time.time()
+    print("Serial Processing")
+    arr = range(100000)
+    ret = []
+
+    for i in arr:
+        ret.append(Square(i))
+    #print("Result of serial processing is ",ret)
+    end = time.time()
+    print("Time requred for serial execution", end-start)
+
+def main():
+
+    print("Inside main")
+    SerialProcessing()
+
+if __name__ == "__main__":
+    main()
